d89f8004751fef689f99645abce777485adfde75
Craig McDonald <craig@mcdonald.codes>
2023-03-22 21:15:16 +0000

    elecom trackball script added

---
+++ elecom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/bin/xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12
+/usr/bin/xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1
+
+/usr/bin/xinput set-prop "pointer:ELECOM TrackBall Mouse EX-G Pro TrackBall Mouse" 'libinput Button Scrolling Button' 11
+/usr/bin/xinput set-prop "pointer:ELECOM TrackBall Mouse EX-G Pro TrackBall Mouse" 'libinput Scroll Method Enabled' 0 0 1
+
